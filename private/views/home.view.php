e51617f548a307acf744fe061839caaa81a19120
DipeshRamStha <visit.dipesh933@gmail.com>
2024-03-08 01:48:39 -0600

    Created the bootstrap css and js in assets folder and linked the min css in home view php file

--- private/views/home.view.php
+++ private/views/home.view.php
@@ -4,8 +4,15 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home</title>
+
+  <link rel="stylesheet" type="text/css" href="http://localhost/school/public/assets/bootstrap.min.css">
+
 </head>
 <body>
-  <h1>This is home</h1>
+
+  <div class="container-fluid">
+
+    <h1>This is home</h1>
+  </div>
 </body>
 </html>
