a6889a473b48e7beb12d31e7bfc35e241ff44a97
DipeshRamStha <visit.dipesh933@gmail.com>
2024-05-03 04:19:04 +0545

    Separated the header and footer from the home view php file into separated includes folder

--- private/views/home.view.php
+++ private/views/home.view.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home</title>
 
-  <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/all.min.css">
-
-</head>
-<body>
-
-  <div class="container-fluid">
-
-    <h1><i class="fa fa-plus"></i>This is home</h1>
-  </div>
-</body>
-</html>
+    <div class="container-fluid">
+      <h1><i class="fa fa-plus"></i>This is home</h1>
+    </div>
+ 
