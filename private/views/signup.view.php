c1e47887b4b29f73becbe45ddce2bdc185ae90b0
DipeshRamStha <visit.dipesh933@gmail.com>
2024-05-03 13:07:39 +0545

    Added the form field with Add User and Cancel buttons

--- private/views/signup.view.php
+++ private/views/signup.view.php
@@ -5,11 +5,29 @@
   <h2 class="text-center">My School</h2>
   <img src="<?=ROOT?>/assets/logo.png" class="border border-primary d-block mx-auto rounded-circle" style="width:100px;">
   <h3>Add User</h3>
-  <input class="form-control" type="email" name="email" placeholder="Email" autofocus>
+  <input class="my-2 form-control" type="firstname" name="firstname" placeholder="First Name">
+  <input class="my-2 form-control" type="lastname" name="lastname" placeholder="Last Name">
+  <input class="my-2 form-control" type="email" name="email" placeholder="Email">
+
+  <select class="my-2 form-control">
+    <option>--Select a Gender--</option>
+    <option>Male</option>
+    <option>Female</option>
+  </select>
+  <select class="my-2 form-control">
+    <option value="">--Select a Rank--</option>
+    <option value="student">Student</option>
+    <option value="reception">Reception</option>
+    <option value="lecturer">Lecturer</option>
+    <option value="admin">Admin</option>
+    <option value="super_admin">Super Admin</option>
+  </select>
+
+  <input class="my-2 form-control" type="text" name="password" placeholder="Password">
+  <input class="my-2 form-control" type="text" name="password2" placeholder="Retype Password">
   <br>
-  <input class="form-control" type="password" name="password" placeholder="Password">
-  <br>
-  <button class="btn btn-primary">Add User</button>
+  <button class="btn btn-primary float-end">Add User</button>
+  <button class="btn btn-danger">Cancel</button>
 </div>
 </div>
 <?php $this->view('includes/footer')?>
