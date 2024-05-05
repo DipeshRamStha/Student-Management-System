9b2bd4ad038d78e96c0762566bf5a2b26e6b8b05
DipeshRamStha <visit.dipesh933@gmail.com>
2024-05-05 07:12:19 +0545

    Included the bread crumbs in the profile page and added images

---
+++ private/controllers/Profile.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Profile Controller
+ */
+class Profile extends Controller
+{
+  function index()
+  {
+    // code...
+    $this->view('profile');
+  }
+
+}
---
+++ private/views/profile.view.php
@@ -0,0 +1,14 @@
+<?php $this->view('includes/header')?>
+<?php $this->view('includes/nav')?>
+  <div class="container-fluid p-4 shadow mx-auto" style="max-width: 1000px;">
+    <?php $this->view('includes/crumbs')?>
+
+    <h4>Profile</h4>
+    <div class="row">
+      <div class="col-4"></div>
+      <div class="col-auto bg-secondary"></div>
+    </div>
+
+  </div>
+
+<?php $this->view('includes/footer')?>
