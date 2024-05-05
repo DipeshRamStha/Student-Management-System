31a4481fea92627bf36769d47389489610252d9d
DipeshRamStha <visit.dipesh933@gmail.com>
2024-05-05 07:31:44 +0545

    Added the profile Details in the profile view php file with the name and gender

--- private/views/profile.view.php
+++ private/views/profile.view.php
@@ -3,12 +3,21 @@
   <div class="container-fluid p-4 shadow mx-auto" style="max-width: 1000px;">
     <?php $this->view('includes/crumbs')?>
 
-    <h4>Profile</h4>
     <div class="row">
       <div class="col-4">
-        <img src="<?=ASSETS?>/user_female.jpg">
+        <img src="<?=ASSETS?>/user_female.jpg" class="border border-primary d-block mx-auto rounded-circle" style="width:150px;">
       </div>
-      <div class="col-8 bg-secondary"></div>
+      <div class="col-8 bg-light p-2">
+        <table class="table table-hover table-striped table-bordered">
+          <tr><th>First Name:</th><td>Mary</td></tr>
+          <tr><th>Last Name:</th><td>Phiri</td></tr>
+          <tr><th>Gender:</th><td>Female</td></tr>
+        </table>
+      </div>
+    </div>
+
+    <div>
+      
     </div>
 
   </div>
