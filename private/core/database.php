09fcb48f591c874bb167dca299569f74a79e9d11
DipeshRamStha <visit.dipesh933@gmail.com>
2024-05-14 16:17:15 +0545

    Defined the class Database with the function definition of connect and run to connect the database and fetch data

--- private/core/database.php
+++ private/core/database.php
@@ -1,3 +1,44 @@
 <?php
 
+/**\
+ *  Database Connection
+ */
 
+class Database
+{
+  private function connect()
+  {
+    $string = "mysql:host=localhost;dbname=school_db";
+    if(!$con = new PDO($string, 'root', '')){
+      die("Could not connect to database");
+    }
+    return $con;
+  }
+
+  private function run($query, $data = array(), $data_type="object")
+  {
+    $con = $this->connect();
+    $stm = $con->prepare($query);
+
+    if($stm) {
+      $check = $stm->execute($data);
+      if($check){
+        if($data_type == "object"){
+          $data = $stm->fetchAll(PDO::FETCH_OBJ);
+        } else {
+          $data = $stm->fetchAll(PDO::FETCH_ASSOC);
+        }
+        if(is_array($data) && count($data) >0){
+          return $data;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  public function query()
+  {
+
+  }
+}
