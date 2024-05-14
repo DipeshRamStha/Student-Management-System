db61566af41212ca18a886c9960a9478cd797c82
DipeshRamStha <visit.dipesh933@gmail.com>
2024-05-14 07:33:20 +0545

    Added the routing to profile view pages to the nav view php pages

--- private/views/profile.view.php
+++ private/views/profile.view.php
@@ -21,28 +21,29 @@
 
     <div class="container-fluid">
       <ul class="nav nav-tabs">
-  <li class="nav-item">
-    <a class="nav-link active" href="#">Basic Info</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Classes</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Tests</a>
-  </li>
-  
-</ul>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Basic Info</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Classes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tests</a>
+        </li>
+        
+        
+      </ul>
 
-<nav class="navbar navbar-light bg-light">
-  <form class="form-inline">
-    <div class="input-group">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">@</span>
-      </div>
-      <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-    </div>
-  </form>
-</nav>
+      <nav class="navbar navbar-light bg-light">
+        <form class="form-inline">
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon1"><i class="fa fa-search">&nbsp</i></span>
+            </div>
+            <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1">
+          </div>
+        </form>
+      </nav>
       
     </div>
 
